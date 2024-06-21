51ed920697ac8fd1447e34be1c9680337e5dfde5
Clarice Mendes <98920251+Claricez@users.noreply.github.com>
2024-06-21 10:21:25 -0300

    fix: ajustes finais

--- assets/js/main-sobre.js
+++ assets/js/main-sobre.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname.includes('sobre-pokemon.html')) {
         console.log('Script executado na página correta.');
 
-        const pokemonSobre = document.getElementById('pokemon-sobre');
+        const pokemonSobre = document.getElementById('pokemon');
         if (!pokemonSobre) {
             console.error('Elemento com ID "pokemon-sobre" não encontrado na página.');
             return;
@@ -18,22 +18,37 @@ document.addEventListener('DOMContentLoaded', () => {
             pokeApiSobre.getPokemon(id).then((pokemon) => {
                 console.log('Fetched Pokémon details:', pokemon);
                 const newHtml = `
+                <div >
+                    <div class="menu-navigation">
+                    <a href="../../index.html">Retornar</a>
+                      <p class="id-pokemon">#${pokemon.number}</p>
+                    <a>Like</a>
+                </div>
+                <div class="content-details" id="pokemon-sobre">
                     <div class="image-pokemon pokemon-item" data-id="${pokemon.number}">
-                        <img src="${pokemon.photo}" alt="${pokemon.name}" />
+                        <img src="${pokemon.photo}" alt="${pokemon.name}" class="photo-pokemon"/>
                     </div>
-                    <div class="details">
-                        <h2>${pokemon.name}</h2>
+                    <h2>${pokemon.name}</h2>
                         <div class="details-list">
                             <ol class="types">
                                 ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                             </ol>
                         </div>
-                        <p>Number: ${pokemon.number}</p>
-                        <p>Type: ${pokemon.type}</p>
-                        <p>Height: ${pokemon.height}</p>
-                        <p>Weight: ${pokemon.weight}</p>
-                        <p>Abilities: ${pokemon.abilities.join(', ')}</p>
-                    </div>`;
+                </div>
+                <div class="description-content ${pokemon.type}">
+                    <p class="about-title">About</p>
+                    <div class="details">
+                        
+                      
+                        <p><b>Type:</b> ${pokemon.type}</p>
+                        <p><b>Height: </b> ${pokemon.height}</p>
+                        <p><b>Weight:</b> ${pokemon.weight}</p>
+                        <p><b>Abilities:</b> ${pokemon.abilities.join(', ')}</p>
+                    </div>
+                </div>
+                </div>
+                    `
+                    
                 
                 pokemonSobre.innerHTML = newHtml;
             }).catch((error) => {
